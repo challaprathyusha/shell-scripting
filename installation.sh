2dd56257e2ab909c731a3a3c9afb7e8722fb4f3e
challa prathyusha <cprathyusha94@gmail.com>
2024-04-17 01:00:46 -0500

    adding new file

--- installation.sh
+++ installation.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-sudo dnf install mysql -y
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "run the command only with super user privilages"
+else
+    echo "you are super user"
+fi
+
+dnf install mysql -y
