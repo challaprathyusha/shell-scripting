b6bd269c3a7d78bac95ccb6481f7243c8f13df43
challa prathyusha <cprathyusha94@gmail.com>
2024-04-16 22:56:01 -0500

    adding new file

---
+++ shellscript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh:Hi Suresh.How are you?"
+echo "Suresh:Hi Ramesh, I am good. What about you?"
+echo "Ramesh:I am good. What are you up to?"
+echo "Suresh:I am learning devops Ramesh."
---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:Hi $PERSON2.How are you?"
+echo "$PERSON2:Hi $PERSON1, I am good. What about you?"
+echo "$PERSON1:I am good. What are you up to?"
+echo "$PERSON2:I am learning devops $PERSON1."
