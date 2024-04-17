aebf9d28e6b871248f37df1db16d028140e61a5b
challa prathyusha <cprathyusha94@gmail.com>
2024-04-16 23:57:02 -0500

    adding new file

---
+++ specialvar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "to print all variables:$@"
+echo "to print no of var passed in script:$#"
+echo "script name:$0"
+echo "current working dir:$PWD"
+echo "home dir of current user:$HOME"
+echo "which user running the script:$USER"
+echo "hostname:$HOSTNAME"
+echo "PID of current running script:$$"
+echo "PID of last background command:$!"
+
