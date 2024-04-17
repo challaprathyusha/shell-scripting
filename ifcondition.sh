2bfd6219fd153fe53354432a1ec10c47ff349e5b
challa prathyusha <cprathyusha94@gmail.com>
2024-04-17 00:31:11 -0500

    adding new file

--- ifcondition.sh
+++ ifcondition.sh
@@ -3,7 +3,7 @@ number=$1
 
 if [ $number -ne 10 ]
 then 
-    echo "given $number is not equal to 10"
+    echo "given number $number is not equal to 10"
 else
-    echo "given $number is equals to 10"
+    echo "given number $number is equals to 10"
 fi
