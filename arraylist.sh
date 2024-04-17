0bc851c619c5b3205ac41e2d16c5790032224e9d
challa prathyusha <cprathyusha94@gmail.com>
2024-04-16 23:25:54 -0500

    adding new file

---
+++ arraylist.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+movies=("bahubali","animal","RRR","sye","fidaa")
+
+echo "print first movie name ::${movies[0]}"
+echo "print second movie name ::${movies[1]}"
+echo "print fifth movie name ::${movies[4]}"
+echo "print all movie names ::${movies[@]}"
