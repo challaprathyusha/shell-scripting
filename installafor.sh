dede9483f098075edc3e4b8fcd76beddf41c4eba
challa prathyusha <cprathyusha94@gmail.com>
2024-04-19 00:07:24 -0500

    adding new file

--- installafor.sh
+++ installafor.sh
@@ -4,33 +4,39 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0|cut -d "." -f1)
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
-G="\e[32m"
+G="\e[33m"
+Y="\e[32m
 N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then 
     echo "you dont have super user previlages to run the command"
     exit 12
+elase
+    echo"You are super user"
 fi
 
 validate(){
-
   if [ $1 -ne 0 ] 
   then
-    echo -e "$2 installation....$R FAILURE $N"
+    echo -e "$2 installation is....$R FAILURE $N"
     exit 1
   else
-    echo -e  "$2 installation.....$G SUCCESS $N"
+    echo -e  "$2 installation is.....$G SUCCESS $N"
   fi  
 }
 
 for i in $@
 do
-dnf list installed $i &>>$LOG_FILE
-validate $? $i
-
-dnf install $i -y &>>$LOG_FILE
-validate $? $i
+    echo "package to install:$i"
+    dnf list installed $i &>>$LOG_FILE
+    if [ $? -eq 0 ]
+    then
+        echo -e " $i is already installed.....$Y SKIPPING $N"
+    else
+        dnf install $i -y &>>$LOG_FILE
+        validate $? $i
+    fi
 done
 
 
