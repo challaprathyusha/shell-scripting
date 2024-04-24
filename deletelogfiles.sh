ad2796ba67d929a79465b1ab111a600581eb59c9
challa prathyusha <cprathyusha94@gmail.com>
2024-04-24 15:31:24 -0500

    adding files to the git

---
+++ deletelogfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SOURCEFOLDER=/tmp/applogfiles
+
+if [ -d $SOURCEFOLDER ]
+then 
+    echo "the directory $SOURCEFOLDER exist"
+else
+    echo "Please make sure $SOURCEFOLDER exist"
+    exit 1
+fi 
+
+FILES=find . type -f -name "*.log" -mtime +14
+
+echo "files that are more than 14 days old:$FILES"
