a26e2e61f61c56f6e8925fe0bc2ee1869eadf73a
challa prathyusha <cprathyusha94@gmail.com>
2024-04-24 15:40:10 -0500

    adding files to the git

--- deletelogfiles.sh
+++ deletelogfiles.sh
@@ -9,7 +9,7 @@ else
     echo "Please make sure $SOURCEFOLDER exist"
     exit 1
 fi 
+cd $SOURCEFOLDER
 
 FILES=${find . type -f -name "*.log" -mtime +14}
-
 echo "files that are more than 14 days old:$FILES"
