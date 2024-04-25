78c3a090c57bffcfb10e6c21c9a0d25752240019
challa prathyusha <cprathyusha94@gmail.com>
2024-04-25 15:49:47 -0500

    adding files to the git

--- wordcount.sh
+++ wordcount.sh
@@ -5,7 +5,7 @@ FILE=/home/ec2-user/shell-scripting/textfile
 #cat $FILE |grep -o 'word'|wc -l
 #cat $FILE| uniq
 k=$(cat $FILE)
-VARIABLE=$(echo $k| tr " " "\n" | sort | uniqs)
+VARIABLE=$(k| tr " " "\n" | sort | uniqs)
 #echo $k | uniq
 
 
