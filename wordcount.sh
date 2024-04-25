2433ed17aeca7b3f15ff37ba69bfa71aa2d6d36a
challa prathyusha <cprathyusha94@gmail.com>
2024-04-25 16:17:32 -0500

    adding files to the git

--- wordcount.sh
+++ wordcount.sh
@@ -4,7 +4,6 @@ FILE=/home/ec2-user/shell-scripting/textfile
 
 #cat $FILE |grep -o 'word'|wc -l
 #cat $FILE| uniq
-s=$(cat $FILE| tr " " "\n")
 k=$(cat $FILE| tr " " "\n" | sort | uniq)
 echo $k 
 for i in $k
