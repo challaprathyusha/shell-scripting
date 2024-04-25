90d7f5d71ec8b6fb97cfba9402870246dc759d08
challa prathyusha <cprathyusha94@gmail.com>
2024-04-24 23:43:26 -0500

    adding files to the git

--- diskusage.sh
+++ diskusage.sh
@@ -5,8 +5,8 @@ DISKTHRESHOLD=10
 
 while read -r line
 do
-DISKUSAGE=$FILESYSTEM|awk -F " " '{print$6F}'|cut -d "%" -f1
-FOLDER=$FILESYSTEM|awk -F " " '{print$NF}'
+DISKUSAGE=$line|awk -F " " '{print$6F}'|cut -d "%" -f1
+FOLDER=$line|awk -F " " '{print$NF}'
 if [ $DISKUSAGE -ge $DISKTHRESHOLD ]
 then    
     echo "$FOLDER is $DISKUSAGE, which is greater then $DISKTHRESHOLD"
