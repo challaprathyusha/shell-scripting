5656083dffde6ac77c44fc63dcb4239f1cdf366a
challa prathyusha <cprathyusha94@gmail.com>
2024-04-25 09:46:34 -0500

    adding files to the git

--- wordcount.sh
+++ wordcount.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 FILE=$1
-
- for word in $FILE
- do 
-    echo $word
-    grep -o $word $FILE
-    grep -o $word $FILE|wc -l
-done
+while
+do
+    for word in $FILE
+    do 
+        echo $word
+        grep -o $word $FILE
+        grep -o $word $FILE|wc -l
+    done
+done <<< $FILE 
 
 
