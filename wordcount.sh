95c5ae6a3e577aa410afa6e6831f4d5a692bce7c
challa prathyusha <cprathyusha94@gmail.com>
2024-04-25 16:14:29 -0500

    adding files to the git

--- wordcount.sh
+++ wordcount.sh
@@ -4,14 +4,12 @@ FILE=/home/ec2-user/shell-scripting/textfile
 
 #cat $FILE |grep -o 'word'|wc -l
 #cat $FILE| uniq
-#cat $FILE
+cat $FILE| tr " " "\n"
 k=$(cat $FILE| tr " " "\n" | sort | uniq)
 echo $k 
-while read -r word
+for i in $k
 do
-    grep -o $word 'cat $FILE'|wc -l
-    echo "$word:grep -o $word 'cat $FILE'|wc -l"
-done <<<  'cat $FILE| tr " " "\n" | sort | uniq'
+    grep -o $i 
 
 
 
