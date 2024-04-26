9c42206ac9a9bd47617ff76dbace0c015e3ab51b
challa prathyusha <cprathyusha94@gmail.com>
2024-04-25 20:02:52 -0500

    adding files to the git

--- rowstocolumns.sh
+++ rowstocolumns.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 FILE="/home/ec2-user/shell-scripting/txtfile"
-cat $FILE
-k=$(cat $FILE|tr -s '[:blank:]' '\n')
+s=$(cat $FILE)
+#k=$(cat $FILE|tr -s '[:blank:]' '\n')
 
 #echo $s|awk -F "
-
+while read -r line
+do
+  k=$(cat $FILE|tr -s '[:blank:]' '\n')
+done <<< echo $s
 
