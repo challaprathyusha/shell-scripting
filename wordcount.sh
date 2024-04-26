b9633bc2d144a4a7cd0cc16512da9b3112f6f883
challa prathyusha <cprathyusha94@gmail.com>
2024-04-26 00:28:05 -0500

    adding files to the git

--- wordcount.sh
+++ wordcount.sh
@@ -12,7 +12,7 @@ do
     echo "$word:$occurence"
 done
 
-data=$(echo "$word:$occurence")
-sorted_data=$(echo "$data" | sort -t ':' -k2,2rn)
+echo "$word:$occurence"
+sorted_data=$(echo "$word:$occurence"| sort -t ':' -k2,2rn)
 echo "$sorted_data"
 
